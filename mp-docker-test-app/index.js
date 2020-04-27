2a42be165fe4e2f3f35eb4efe9550b1e7a8e4306
Akshay Kumar M P <akshaykumar.mp@amadeus.com>
2020-04-27 20:06:51 +0530

    initial

---
+++ mp-docker-test-app/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Success Response');
+});
+
+app.listen(3001, err => console.log('App is listening to port 3001'))
