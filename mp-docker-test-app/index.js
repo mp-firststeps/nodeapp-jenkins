1a812325ce44f50cf1760cb0bfdc482040896edf
mpakshaykumar <mpakshay.coder@gmail.com>
2020-04-27 21:46:16 +0530

    added travis

--- mp-docker-test-app/index.js
+++ mp-docker-test-app/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Success Response');
+    res.send('Success Response :: GIT :: TRAVIS :: EB');
 });
 
 app.listen(3001, err => console.log('App is listening to port 3001'))
